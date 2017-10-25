9934c42002bd3726378c50809bbbe3e7a22886bb
rk1005 <rkumarkurva@gmail.com>
2017-10-25 01:10:49 -0500

    axious api in one single page

--- app-promise.js
+++ app-promise.js
@@ -1,23 +1,39 @@
+/* modules importation */
 const yargs = require('yargs');
 const axios = require('axios');
 
-const argv = yargs
-.options({
-a:{
-    demand : true,
-    alias : 'address',
-    describe : 'address to fetch weather',
-    string : true
-}  
+/* input argument's parsing and alias creation */
+
+var args = yargs
+    .options({
+        a:{
+            demand : true,
+            alias : 'address',
+            string : true
+        }
+     }).argv;
+
+var geolocationUrl = `https://maps.google.com/maps/api/geocode/json?address=${args.address}`;
+console.log(geolocationUrl);
+
+axios.get(geolocationUrl)
+.then(function(response){
+    console.log(response.data);
+        console.log(response.data.results[0].formatted_address);
+        var latitude = response.data.results[0].geometry.location.lat;
+        var logitude = response.data.results[0].geometry.location.lng;
+        var weatherUrl = `https://api.darksky.net/forecast/53090cd0e9f4c18f5a4416ac304a2b47/${latitude},${logitude}`
+        console.log(weatherUrl)
+        // console.log(lat, lng);
+        return axios.get(weatherUrl)
+        .then(function(response){
+            console.log(response.data.currently.temperature);
+            console.log(response.data.currently.apparentTemperature)
+            // console.log(typeof response)
+        }).catch(function(e){
+            console.log(e)
+        });
 })
-.help()
-.alias('help', 'h')
-.argv;
-var encodedAddress = encodeURIComponent(argv.address);
-var geocodeUrl = `https://maps.google.com/maps/api/geocode/json?address=${encodedAddress}`
-console.log(geocodeUrl);
-// axios.get(geocodeUrl).then((res)=>{
-//     console.log(res);
-// }).catch((err)=>{
-//     console.log(err);
-// });
+.catch(function(error){
+        console.log(error);
+});
